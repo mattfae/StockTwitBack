49107a3aed642408367ec008050c31c4b67ab363
msguttman <msguttman@gmail.com>
2021-02-18 13:02:55 -0600

    working data collection

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 Python Flask backend for a web-app to predict stock-prices based on sentiment analysis of historical tweets.
 
+Enter a given stock symbol and date to see the stock's opening price on that date compared with the previous closing price, and a record of tweets about the stock during the afterhours period before close and open.
 
-# TODO
-
+Uses the tweepy library to gather twitter API data, and Polygon.io to gather stock price data.
--- StockTwit/analyses.py
+++ StockTwit/analyses.py
@@ -1,8 +1,12 @@
 import tweepy
+import requests
+import json
+import pdb
 from decouple import config
 
 CONSUMER_KEY = config('TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET_KEY = config('TWITTER_CONSUMER_SECRET_KEY')
+POLYGON_API_KEY = config('POLYGON_API_KEY')
 
 tweepy_auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
 api = tweepy.API(tweepy_auth)
@@ -16,8 +20,22 @@ class StockTweets:
         self.stock_symbol = f'${_stock_symbol}'
         self.open_date = _open_date
         self.tweets = list()
+        self.close_price = None
+        self.open_price = None
 
     def get_tweets(self):
-        for tweet in tweepy.Cursor(api.search, q=self.stock_symbol, until=self.open_date, count=10):
+        #use tweepy to geat relevant tweets
+        print("starting get_tweets")
+        for tweet in tweepy.Cursor(api.search, q=self.stock_symbol, until=self.open_date, count=10).items(10):
             print("printing tweet", tweet.text)
             self.tweets.append(tweet.text)
+
+    def get_close_open(self):
+        close_response = requests.get('https://api.polygon.io/v1/open-close/AAPL/2020-10-14?unadjusted=false&apiKey=Ua0sj7CCQxDQfCXLVp34CFUKWyVCWL99')
+        self.close_price = json.loads(close_response.content.decode('utf-8'))["close"]
+        
+        open_response = requests.get('https://api.polygon.io/v1/open-close/AAPL/2020-10-14?unadjusted=false&apiKey=Ua0sj7CCQxDQfCXLVp34CFUKWyVCWL99')
+        self.open_price = json.loads(open_response.content.decode('utf-8'))["open"]
+
+pdb.set_trace()
+#new = StockTweets('aapl', '2021-02-18')
